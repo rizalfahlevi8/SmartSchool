0bc95d0840ce9985e39b941a7cac750eeb143493
Nov189 <125938835+Nov189@users.noreply.github.com>
2023-10-03 20:18:34 +0700

    Update inventaris.blade.php

--- resources/views/pages/sarana/data-inventaris/inventaris.blade.php
+++ resources/views/pages/sarana/data-inventaris/inventaris.blade.php
@@ -1,14 +1,13 @@
-@extends('admin.app')
+@extends('components.main')
 
 @section('title-content')
     Inventaris
 @endsection
 
 @section('breadcrumbs')
-    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="/inventaris">Inventaris</a></li>
-    </ol>
-    <li class="breadcrumb-item text-sm text-dark active" aria-current="page"></li>
+<ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="/dashboard">Dashboard</a></li>
+        <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Inventaris</li>
     </ol>
     <h6 class="font-weight-bolder mb-0">Inventaris</h6>
 @endsection
@@ -24,27 +23,51 @@
                 </div>
                 <div class="card-body px-0 pb-2">
                     <div class="d-flex justify-content-between mb-4">
-                        <a href="{{ route('daftar-barang.index') }}" class="btn btn-primary">Daftar Barang</a>
-                        <a href="{{ route('ruang.index') }}" class="btn btn-primary">Ruang</a>
                     </div>
 
                     <table class="table align-items-center mb-0">
                         <thead>
                             <tr>
-                                <th class="text-center">No</th>
-                                <th class="text-center">ID Ruangan</th>
-                                <th class="text-center">Nama Ruangan</th>
-                                <th class="text-center">Aksi</th>
+                                <th
+                                    class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
+                                        No
+                                    </th>
+                                    <th
+                                    class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
+                                        Nama Ruang
+                                    </th>
+                                    <th
+                                    class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
+                                        Luas
+                                    </th>
+                                    <th
+                                    class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
+                                        Lokasi
+                                    </th>
+                                    <th
+                                    class="
+                                        text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
+                                    Aksi
+                                </th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($dataRuangan as $ruangan)
+                            @foreach ($ruangs as $ruang)
                                 <tr>
-                                    <td class="text-center">{{ $loop->iteration }}</td>
-                                    <td class="text-center">{{ $ruangan->id }}</td>
-                                    <td class="text-center">{{ $ruangan->nama }}</td>
                                     <td class="text-center">
-                                        <a href="{{ route('atur-barang', $ruangan->id) }}" class="btn btn-primary">Atur Barang</a>
+                                        {{ $loop->iteration }}
+                                    </td>
+                                    <td class="text-center">
+                                        {{ $ruang->nama_ruang }}
+                                    </td>
+                                    <td class="text-center">
+                                        {{ $ruang->luas }}
+                                    </td>
+                                    <td class="text-center">
+                                        {{ $ruang->lokasi }}
+                                    </td>
+                                    <td class="text-center">
+                                        <a href="{{ route('atur-barang', $ruang->id) }}" class="btn btn-primary">Atur Barang</a>
                                     </td>
                                 </tr>
                             @endforeach
