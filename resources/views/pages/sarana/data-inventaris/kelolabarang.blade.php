8b9b673ebd96257ac91a85e3446d7210740462b9
Nov189 <125938835+Nov189@users.noreply.github.com>
2023-10-04 08:46:17 +0700

    Update kelolabarang.blade.php

--- resources/views/pages/sarana/data-inventaris/kelolabarang.blade.php
+++ resources/views/pages/sarana/data-inventaris/kelolabarang.blade.php
@@ -10,99 +10,113 @@
         <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Kelola Barang</li>
     </ol>
     <h6 class="font-weight-bolder mb-0">Persediaan barang</h6>
+    <h6 class="font-weight-bolder mb-0">Ruang : {{$ruangs->nama_ruang}}</h6>
 @endsection
 
 @section('content')
-<div class="row">
+    <div class="row">
         <div class="col-12">
             <div class="card my-4">
                 <div class="card-body px-0 pb-2">
                     <div class="table-responsive pb-2 px-3">
                         <div class="row">
+                        
+
                             <div class="col-12">
                                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tambahBarangModal">
                                     Tambah Barang
                                 </button>
                             </div>
                         </div>
-
-                        <!-- Modal for adding a new record -->
-                        <div class="modal fade" id="tambahBarangModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">Tambah Barang</h5>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <!-- Form to add a new record -->
-                                        <form action="{{ route('store-inventaris', $ruangs->id) }}" method="POST">
-                                        @csrf
-                                        <!-- ID Barang -->
-                                        <div class="mb-3">
-                                            <label for="id_daftarbarang" class="form-label">ID Barang</label>
-                                            <input type="number" class="form-control" id="id_daftarbarang" name="id_daftarbarang" required>
-                                        </div>
-                                        <!-- Nama Barang -->
-                                        <div class="mb-3">
-                                            <label for="nama_barang" class="form-label">Nama Barang</label>
-                                            <input type="text" class="form-control" id="nama_barang" name="nama_barang" required>
-                                        </div>
-                                        <!-- Tahun Pengadaan -->
-                                        <div class="mb-3">
-                                            <label for="tahun_pengadaan" class="form-label">Tahun Pengadaan</label>
-                                            <input type="date" class="form-control" id="tahun_pengadaan" name="tahun_pengadaan">
-                                        </div>
-                                        <!-- Jenis -->
-                                        <div class="mb-3">
-                                            <label for="jenis" class="form-label">Jenis</label>
-                                            <input type="text" class="form-control" id="jenis" name="jenis" required>
-                                        </div>
-                                        <!-- Jumlah Barang -->
-                                        <div class="mb-3">
-                                            <label for="jumlah_barang" class="form-label">Jumlah Barang</label>
-                                            <input type="number" class="form-control" id="jumlah_barang" name="jumlah_barang" required>
-                                        </div>
-                                        <!-- Jumlah Barang Baik -->
-                                        <div class="mb-3">
-                                            <label for="jumlah_baik" class="form-label">Jumlah Barang Baik</label>
-                                            <input type="number" class="form-control" id="jumlah_baik" name="jumlah_baik" required>
-                                        </div>
-                                        <!-- Jumlah Barang Rusak -->
-                                        <div class="mb-3">
-                                            <label for="jumlah_rusak" class="form-label">Jumlah Barang Rusak</label>
-                                            <input type="number" class="form-control" id="jumlah_rusak" name="jumlah_rusak" required>
-                                        </div>
-                                        <!-- Tombol Submit -->
-                                        <button type="submit" class="btn btn-primary">Tambahkan Barang</button>
-                                    </form>
-                                    <!-- Tabel untuk menampilkan inventaris -->
-                                    <table id="example" class="table align-items-center mb-0">
-                                        <thead>
-                                            <tr>
-                                                <th class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">No</th>
-                                                <th class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">Nama Barang (Daftar Barang)</th>
-                                                <th class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">Jumlah Barang (Inventaris)</th>
-                                                <th class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">Jumlah Barang Rusak (Inventaris)</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <!-- Loop untuk menampilkan data inventaris -->
-                                            @foreach ($inventaris as $i)
-                                            <tr>
-                                                <td class="text-center">{{ $loop->iteration }}</td>
-                                                <!-- Nama Barang dari Daftar Barang -->
-                                                <td class="text-center">{{ $i->daftarbarang->nama_barang ?? '-' }}</td>
-                                                <!-- Jumlah Barang dari Inventaris -->
-                                                <td class="text-center">{{ $i->jumlah_barang ?? '-' }}</td>
-                                                <!-- Jumlah Barang Rusak dari Inventaris -->
-                                                <td class="text-center">{{ $i->jumlah_barang_rusak ?? '-' }}</td>
-                                            </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-
+                        <!-- Tabel untuk menampilkan inventaris -->
+                        <table id="example" class="table align-items-center mb-0">
+                            <thead>
+                                <tr>
+                                    <th class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">No</th>
+                                    <th class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">Nama Barang (Daftar Barang)</th>
+                                    <th class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">Tahun Pengadaan</th>
+                                    <th class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">Jenis</th>
+                                    <th class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">Jumlah Barang</th>
+                                    <th class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">Jumlah Barang Baik</th>
+                                    <th class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">Jumlah Barang Rusak</th>
+                                    <th class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($inventaris as $i)
+                                <tr>
+                                    <td class="text-center">{{ $loop->iteration }}</td>
+                                    <td class="text-center">{{ $i->daftarbarang->nama_barang ?? '-' }}</td>
+                                    <td class="text-center">{{ $i->tahun_pengadaan ?? '-' }}</td>
+                                    <td class="text-center">{{ $i->jenis ?? '-' }}</td>
+                                    <td class="text-center">{{ $i->jumlah_barang ?? '-' }}</td>
+                                    <td class="text-center">{{ $i->jumlah_baik ?? '-' }}</td>
+                                    <td class="text-center">{{ $i->jumlah_rusak ?? '-' }}</td>
+                                    <td class="text-center">
+                                                <button type="button"data-bs-toggle="modal" class="btn btn-warning font-weight-bold btn--edit text-sm rounded-circle"
+                                                    data-bs-toggle="tooltip" data-bs-placement="bottom" title="Edit"">
+                                                    <i class="fa fa-edit"></i>
+                                                </button>
+                                                <a class=" btn btn-danger font-weight-bold text-sm rounded-circle"
+                                                    data-bs-toggle="tooltip" data-bs-placement="bottom" title="Hapus">
+                                                    <i class="fa fa-trash"></i>
+                                                </a>
+                                            </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal for adding a new record -->
+    <div class="modal fade" id="tambahBarangModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Tambah Barang</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <!-- Form to add a new record -->
+                    <form action="{{ route('store-inventaris', $ruangs->id) }}" method="POST">
+                        @csrf
+                        <!-- Nama Barang -->
+                        <div class="mb-3">
+                            <label for="nama_barang" class="form-label">Nama Barang</label>
+                            <input type="text" class="form-control" id="nama_barang" name="nama_barang" required>
+                        </div>
+                        <!-- Tahun Pengadaan -->
+                        <div class="mb-3">
+                            <label for="tahun_pengadaan" class="form-label">Tahun Pengadaan</label>
+                            <input type="date" class="form-control" id="tahun_pengadaan" name="tahun_pengadaan">
+                        </div>
+                        <!-- Jenis -->
+                        <div class="mb-3">
+                            <label for="jenis" class="form-label">Jenis</label>
+                            <input type="text" class="form-control" id="jenis" name="jenis" required>
+                        </div>
+                        <!-- Jumlah Barang -->
+                        <div class="mb-3">
+                            <label for="jumlah_barang" class="form-label">Jumlah Barang</label>
+                            <input type="number" class="form-control" id="jumlah_barang" name="jumlah_barang" required>
+                        </div>
+                        <!-- Jumlah Barang Baik -->
+                        <div class="mb-3">
+                            <label for="jumlah_baik" class="form-label">Jumlah Barang Baik</label>
+                            <input type="number" class="form-control" id="jumlah_baik" name="jumlah_baik" required>
+                        </div>
+                        <!-- Jumlah Barang Rusak -->
+                        <div class="mb-3">
+                            <label for="jumlah_rusak" class="form-label">Jumlah Barang Rusak</label>
+                            <input type="number" class="form-control" id="jumlah_rusak" name="jumlah_rusak" required>
+                        </div>
+                        <!-- Tombol Submit -->
+                        <button type="submit" class="btn btn-primary">Tambahkan Barang</button>
+                    </form>
                 </div>
             </div>
         </div>
