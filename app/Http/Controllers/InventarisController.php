653a91e3e7eb035303d4c38c336db6be880dba98
Nov189 <125938835+Nov189@users.noreply.github.com>
2023-10-04 08:51:13 +0700

    Update InventarisController.php
    perbaikan controller inventaris

--- app/Http/Controllers/InventarisController.php
+++ app/Http/Controllers/InventarisController.php
@@ -6,6 +6,7 @@ use App\Models\Ruang;
 use App\Models\Inventaris;
 use App\Models\Barang;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB; 
 
 class InventarisController extends Controller
 {
@@ -13,7 +14,7 @@ class InventarisController extends Controller
     {
         $ruang = Ruang::all();
         return view('pages.sarana.data-inventaris.inventaris', [
-            'ruangs'      => $ruang
+            'ruangs' => $ruang,
         ])->with('title', 'Daftar Inventaris');
     }
 
@@ -23,21 +24,19 @@ class InventarisController extends Controller
         $inventaris = Inventaris::where('ruang_id', $id)->get();
 
         return view('pages.sarana.data-inventaris.kelolabarang', [
-            'ruangs'      => $ruang,
-            'inventaris'  => $inventaris
+            'ruangs' => $ruang,
+            'inventaris' => $inventaris,
         ])->with('title', 'Daftar Inventaris');
     }
-
-
 
     public function store(Request $request, $id)
     {
         // Validasi request
         $request->validate([
-            'id_daftarbarang' => 'required|numeric',
             'nama_barang' => 'required|string|max:255',
             'tahun_pengadaan' => 'required|date',
             'jenis' => 'required|string|max:255',
+            'jumlah_barang' => 'required|integer|min:0',
             'jumlah_baik' => 'required|integer|min:0',
             'jumlah_rusak' => 'required|integer|min:0',
         ]);
@@ -45,22 +44,39 @@ class InventarisController extends Controller
         // Hitung jumlah_barang berdasarkan jumlah_baik dan jumlah_rusak
         $jumlah_barang = $request->jumlah_baik + $request->jumlah_rusak;
 
-        // Simpan data inventaris
-        Inventaris::create([
-            // 'id_ruang' => $id,
-            'id_daftarbarang' => $request->id_daftarbarang,
-            'nama_barang' => $request->nama_barang,
-            'tahun_pengadaan' => $request->tahun_pengadaan,
-            'jenis' => $request->jenis,
-            'jumlah_barang' => $jumlah_barang,
-            'jumlah_baik' => $request->jumlah_baik,
-            'jumlah_rusak' => $request->jumlah_rusak,
-        ]);
+        try {
+            // Mulai transaksi
+            DB::beginTransaction();
 
-        // Perbarui variabel $ruang untuk digunakan pada view
-        $ruang = Ruang::findOrFail($id);
+            // Simpan data inventaris
+            $inventaris = Inventaris::create([
+                'nama_barang' => $request->nama_barang,
+                'tahun_pengadaan' => $request->tahun_pengadaan,
+                'jenis' => $request->jenis,
+                'jumlah_barang' => $jumlah_barang,
+                'jumlah_baik' => $request->jumlah_baik,
+                'jumlah_rusak' => $request->jumlah_rusak,
+            ]);
 
-        return redirect()->route('atur-barang', $id)->with('success', 'Barang berhasil ditambahkan.');
+            // Simpan data ke tabel barangs
+            Barang::create([
+                'nama_barang' => $request->nama_barang,
+                'tahun_pengadaan' => $request->tahun_pengadaan,
+                'jenis' => $request->jenis,
+                'jumlah_barang' => $jumlah_barang,
+                
+            ]);
+
+            // Commit transaksi
+            DB::commit();
+
+            return redirect()->route('inventaris_main')->with('toast_success', 'Data inventaris Berhasil di Tambahkan');
+        } catch (\Exception $e) {
+            // Rollback transaksi jika terjadi exception
+            DB::rollBack();
+
+            return redirect()->back()->with('error', 'Gagal menyimpan data: ' . $e->getMessage());
+        }
     }
 
     public function hapusBarang($id)
