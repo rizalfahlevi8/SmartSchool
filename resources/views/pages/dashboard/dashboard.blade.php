446d782259a0cbdde6d380250729e1c4088b6136
Rynare <rynare.of.legend@gmail.com>
2023-10-22 01:10:19 +0700

    memperbaiki view guru

--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -12,134 +12,146 @@
     @include('components.dashboard.statistic')
 
     @if (auth()->user()->hasRole('guru'))
-        <div class="row">
-            <div class="col-lg-12 col-md-6 mb-4">
-                <div class="card z-index-2 ">
-                    <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
-                        <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
-                            <div class="chart">
-                                {{-- <canvas id="chart-bars" class="chart-canvas" height="170"></canvas> --}}
-                                <h6 class="text-white text-capitalize ps-3">Data Guru
-                                    {{-- {{ $kelas->guru_id ? 'ada' : 'tidak' }} --}}
-                                </h6>
-                            </div>
-                        </div>
+        @if ($myData == null)
+            <div class="row">
+                <div class="col-lg-12 col-md-6 mb-4">
+                    <div class="card z-index-2 ">
+                        <h4 style="text-align: center; width: 100%; padding: 40px 10px">Anda Tidak
+                            memiliki informasi pribadi
+                        </h4>
                     </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="foto">
-                                    <img src="{{ asset('storage/guru/img/' . $myData?->foto) ?? asset('storage/guru/img/default_img.png') }}"
-                                        alt="" width="100%" height="auto">
+                </div>
+            </div>
+        @else
+            <div class="row">
+                <div class="col-lg-12 col-md-6 mb-4">
+                    <div class="card z-index-2 ">
+                        <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
+                            <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                                <div class="chart">
+                                    {{-- <canvas id="chart-bars" class="chart-canvas" height="170"></canvas> --}}
+                                    <h6 class="text-white text-capitalize ps-3">Data Guru
+                                        {{-- {{ $kelas->guru_id ? 'ada' : 'tidak' }} --}}
+                                    </h6>
                                 </div>
                             </div>
-                            <div class="col-md-8">
-                                <ul class="list-group">
-                                    <li class="list-group-item">
-                                        <div class="row">
-                                            <div class="col-md-5">
-                                                <span class="float-start fw-bold">NIP</span>
-                                                <div class="float-end">:</div>
-                                            </div>
-                                            <div class="col-md-7">
-                                                {{ $myData->nip ?? '' }}
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <div class="row">
-                                            <div class="col-md-5">
-                                                <span class="float-start fw-bold">Nama</span>
-                                                <div class="float-end">:</div>
-                                            </div>
-                                            <div class="col-md-7">
-                                                {{ $myData->nama ?? '' }}
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <div class="row">
-                                            <div class="col-md-5">
-                                                <span class="float-start fw-bold">Jenis
-                                                    Kelamin</span>
-                                                <div class="float-end">:</div>
-                                            </div>
-                                            <div class="col-md-7">
-                                                {{ $myData->jenis_kelamin ?? '' }}
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <div class="row">
-                                            <div class="col-md-5">
-                                                <span class="float-start fw-bold">Tempat,
-                                                    tanggal
-                                                    lahir</span>
-                                                <div class="float-end">:</div>
-                                            </div>
-                                            <div class="col-md-7">
-                                                {{ $myData->tempat_lahir ?? '' }}
-                                                @if ($myData->tanggal_lahir)
-                                                    {{ \Carbon\Carbon::parse($myData->tanggal_lahir)->format('d-m-Y') ?? '' }}
-                                                @endif
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <div class="row">
-                                            <div class="col-md-5">
-                                                <span class="float-start fw-bold">No Telepon
-                                                </span>
-                                                <div class="float-end">:</div>
-                                            </div>
-                                            <div class="col-md-7">
-                                                {{ $myData->no_telp ?? '' }}
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <div class="row">
-                                            <div class="col-md-5">
-                                                <span class="float-start fw-bold">Agama</span>
-                                                <div class="float-end">:</div>
-                                            </div>
-                                            <div class="col-md-7">
-                                                {{ $myData->agama ?? '' }}
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <div class="row">
-                                            <div class="col-md-5">
-                                                <span class="float-start fw-bold">Alamat</span>
-                                                <div class="float-end">:</div>
-                                            </div>
-                                            <div class="col-md-7">
-                                                {{ $myData->alamat ?? '' }}
-                                            </div>
-                                        </div>
-                                    </li>
-                                    @if ($myData)
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="foto">
+                                        <img src="{{ asset('storage/guru/img/' . $myData?->foto) ?? asset('storage/guru/img/default_img.png') }}"
+                                            alt="" width="100%" height="auto">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <ul class="list-group">
                                         <li class="list-group-item">
                                             <div class="row">
                                                 <div class="col-md-5">
-                                                    <span class="float-start fw-bold">Wali Kelas</span>
+                                                    <span class="float-start fw-bold">NIP</span>
                                                     <div class="float-end">:</div>
                                                 </div>
                                                 <div class="col-md-7">
-                                                    {{ $myData->kelas->nama_kelas ?? '' }}
+                                                    {{ $myData->nip ?? '' }}
                                                 </div>
                                             </div>
                                         </li>
-                                    @else
-                                    @endif
-                                </ul>
+                                        <li class="list-group-item">
+                                            <div class="row">
+                                                <div class="col-md-5">
+                                                    <span class="float-start fw-bold">Nama</span>
+                                                    <div class="float-end">:</div>
+                                                </div>
+                                                <div class="col-md-7">
+                                                    {{ $myData->nama ?? '' }}
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <div class="row">
+                                                <div class="col-md-5">
+                                                    <span class="float-start fw-bold">Jenis
+                                                        Kelamin</span>
+                                                    <div class="float-end">:</div>
+                                                </div>
+                                                <div class="col-md-7">
+                                                    {{ $myData->jenis_kelamin ?? '' }}
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <div class="row">
+                                                <div class="col-md-5">
+                                                    <span class="float-start fw-bold">Tempat,
+                                                        tanggal
+                                                        lahir</span>
+                                                    <div class="float-end">:</div>
+                                                </div>
+                                                <div class="col-md-7">
+                                                    {{ $myData->tempat_lahir ?? '' }}
+                                                    @if ($myData->tanggal_lahir)
+                                                        {{ \Carbon\Carbon::parse($myData->tanggal_lahir)->format('d-m-Y') ?? '' }}
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <div class="row">
+                                                <div class="col-md-5">
+                                                    <span class="float-start fw-bold">No Telepon
+                                                    </span>
+                                                    <div class="float-end">:</div>
+                                                </div>
+                                                <div class="col-md-7">
+                                                    {{ $myData->no_telp ?? '' }}
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <div class="row">
+                                                <div class="col-md-5">
+                                                    <span class="float-start fw-bold">Agama</span>
+                                                    <div class="float-end">:</div>
+                                                </div>
+                                                <div class="col-md-7">
+                                                    {{ $myData->agama ?? '' }}
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <div class="row">
+                                                <div class="col-md-5">
+                                                    <span class="float-start fw-bold">Alamat</span>
+                                                    <div class="float-end">:</div>
+                                                </div>
+                                                <div class="col-md-7">
+                                                    {{ $myData->alamat ?? '' }}
+                                                </div>
+                                            </div>
+                                        </li>
+                                        @if ($myData)
+                                            <li class="list-group-item">
+                                                <div class="row">
+                                                    <div class="col-md-5">
+                                                        <span class="float-start fw-bold">Wali Kelas</span>
+                                                        <div class="float-end">:</div>
+                                                    </div>
+                                                    <div class="col-md-7">
+                                                        {{ $myData->kelas->nama_kelas ?? '' }}
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        @else
+                                        @endif
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endif
     @elseif(auth()->user()->hasRole('siswa'))
         <div class="row">
             <div class="col-lg-12 col-md-6 mb-4">
