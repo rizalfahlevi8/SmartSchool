43bfabe37b7d72973e52b4953a1944a31cb5244e
DimazRM <141558775+DimazRM@users.noreply.github.com>
2023-10-24 04:13:42 +0700

    Tambah migration untuk pengumuman

---
+++ database/migrations/2023_10_23_184925_create_pengumuman_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengumumans', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('message');
+            $table->enum('role', ['admin', 'guru', 'siswa']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengumumans');
+    }
+};
