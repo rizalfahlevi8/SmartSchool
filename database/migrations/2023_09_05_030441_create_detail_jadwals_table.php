73c0e450947cf85934f4e61c493642d1418efcdd
Rizal Fahlevi <rizalfahlevi810@gmail.com>
2023-10-26 09:27:18 +0700

    memperbaiki tampilan jadwal guru

--- database/migrations/2023_09_05_030441_create_detail_jadwals_table.php
+++ database/migrations/2023_09_05_030441_create_detail_jadwals_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,20 +12,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('detail_jadwals', function (Blueprint $table) {
+        Schema::create('jadwals', function (Blueprint $table) {
             $table->id();
-            $table->time('jam_mulai');
-            $table->time('jam_selesai');
-            $table->text('keterangan')->nullable(true);
-            $table->unsignedBigInteger('id_ruang')->nullable();
-            $table->unsignedBigInteger('id_guru');
-            $table->unsignedBigInteger('id_mapel');
-            $table->unsignedBigInteger('id_jadwal');
-            $table->foreign('id_ruang')->references('id')->on('ruangs')->nullOnDelete();
-            $table->foreign('id_guru')->references('id')->on('gurus');
-            $table->foreign('id_mapel')->references('id')->on('mapels');
-            $table->foreign('id_jadwal')->references('id')->on('jadwals')->cascadeOnDelete();
+            $table->enum('status', ['masuk', 'libur'])->nullable(true);
+            $table->text('catatan')->nullable(true);
+            $table->enum('hari', ['senin', 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu', 'minggu']);
             $table->timestamps();
+            $table->unsignedBigInteger('id_kelas');
+            $table->unsignedBigInteger('id_akademik');
+            $table->foreign('id_akademik')->references('id')->on('akademiks');
+            $table->foreign('id_kelas')->references('id')->on('kelas')->cascadeOnDelete();
         });
     }
 
@@ -33,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('detail_jadwals');
+        Schema::dropIfExists('jadwals');
     }
 };
--- resources/views/pages/akademik/data-jadwal-guru/jadwalguru.blade.php
+++ resources/views/pages/akademik/data-jadwal-guru/jadwalguru.blade.php
@@ -9,6 +9,78 @@
     <h6 class="font-weight-bolder mb-0">Jadwal Mengajar</h6>
 @endsection
 @section('content')
+<div class="row">
+    <div class="col-12">
+        <div class="card my-4">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+                    <h6 class="text-white text-capitalize ps-3">Jadwal Hari Ini :
+                        {{ auth()->user()->guru->nama }} ({{ auth()->user()->guru->nip }})
+                        {{-- {{ $guru->nama ?? '' }} ({{ $guru->NIP ?? '' }}) --}}
+                        {{-- {{ $j->mapel->namamapel ?? '' }} --}}
+                    </h6>
+                </div>
+            </div>
+
+            <div class="card-body px-0 pb-2">
+                <div class="table-responsive pb-2 px-3">
+                    <div class="col text-right">
+                    </div>
+                    <!-- Button trigger modal -->
+                    <table class="table align-items-center mb-0">
+                        <thead>
+                            <tr>
+                                <th
+                                    class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
+                                    Hari</th>
+                                <th
+                                    class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
+                                    Mapel</th>
+                                <th
+                                    class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
+                                    Jam
+                                </th>
+                                <th
+                                    class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
+                                    Ruang
+                                </th>
+                                <th
+                                    class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
+                                    Keterangan</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($all_jadwal as $jadwal)
+                                <tr>
+                                    <td class="text-center">
+                                        {{ $jadwal->jadwal->hari }}
+                                    </td>
+
+                                    <td class="text-center">
+                                        {{ $jadwal->mapel->nama_mapel }}
+                                    </td>
+
+                                    <td class="text-center">
+                                        {{ Carbon\Carbon::parse($jadwal->jam_mulai)->format('H:i') }} -
+                                        {{ Carbon\Carbon::parse($jadwal->jam_selesai)->format('H:i') }}
+                                    </td>
+                                    <td class="text-center">
+                                        {{ $jadwal->ruang->nama_ruang }}
+                                    </td>
+
+                                    <td class="text-center">
+                                        {{ $jadwal->keterangan ?? 'Tidak ada' }}
+                                    </td>
+
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
     <div class="row">
         <div class="col-12">
             <div class="card my-4">
@@ -54,23 +126,23 @@
                                 @foreach ($jadwals as $jadwal)
                                     <tr>
 
-                                        <td class="text-center">
+                                        <td class="text-center" style= "background-color: {{ $jadwal->jadwal->hari == $hari_ini ? 'green' : '' }};color: {{ $jadwal->jadwal->hari == $hari_ini ? 'white' : '' }}">
                                             {{ $jadwal->jadwal->hari }}
                                         </td>
 
-                                        <td class="text-center">
+                                        <td class="text-center" style= "background-color: {{ $jadwal->jadwal->hari == $hari_ini ? 'green' : '' }};color: {{ $jadwal->jadwal->hari == $hari_ini ? 'white' : '' }}">
                                             {{ $jadwal->mapel->nama_mapel }}
                                         </td>
 
-                                        <td class="text-center">
+                                        <td class="text-center" style= "background-color: {{ $jadwal->jadwal->hari == $hari_ini ? 'green' : '' }};color: {{ $jadwal->jadwal->hari == $hari_ini ? 'white' : '' }}">
                                             {{ Carbon\Carbon::parse($jadwal->jam_mulai)->format('H:i') }} -
                                             {{ Carbon\Carbon::parse($jadwal->jam_selesai)->format('H:i') }}
                                         </td>
-                                        <td class="text-center">
+                                        <td class="text-center" style= "background-color: {{ $jadwal->jadwal->hari == $hari_ini ? 'green' : '' }};color: {{ $jadwal->jadwal->hari == $hari_ini ? 'white' : '' }}">
                                             {{ $jadwal->ruang->nama_ruang }}
                                         </td>
 
-                                        <td class="text-center">
+                                        <td class="text-center" style= "background-color: {{ $jadwal->jadwal->hari == $hari_ini ? 'green' : '' }};color: {{ $jadwal->jadwal->hari == $hari_ini ? 'white' : '' }}">
                                             {{ $jadwal->keterangan ?? 'Tidak ada' }}
                                         </td>
 
@@ -83,77 +155,6 @@
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="card my-4">
-                <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                    <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                        <h6 class="text-white text-capitalize ps-3">Jadwal Hari Ini :
-                            {{ auth()->user()->guru->nama }} ({{ auth()->user()->guru->nip }})
-                            {{-- {{ $guru->nama ?? '' }} ({{ $guru->NIP ?? '' }}) --}}
-                            {{-- {{ $j->mapel->namamapel ?? '' }} --}}
-                        </h6>
-                    </div>
-                </div>
 
-                <div class="card-body px-0 pb-2">
-                    <div class="table-responsive pb-2 px-3">
-                        <div class="col text-right">
-                        </div>
-                        <!-- Button trigger modal -->
-                        <table class="table align-items-center mb-0">
-                            <thead>
-                                <tr>
-                                    <th
-                                        class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
-                                        Hari</th>
-                                    <th
-                                        class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
-                                        Mapel</th>
-                                    <th
-                                        class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
-                                        Jam
-                                    </th>
-                                    <th
-                                        class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
-                                        Ruang
-                                    </th>
-                                    <th
-                                        class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
-                                        Keterangan</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($all_jadwal as $jadwal)
-                                    <tr>
-                                        <td class="text-center">
-                                            {{ $jadwal->jadwal->hari }}
-                                        </td>
-
-                                        <td class="text-center">
-                                            {{ $jadwal->mapel->nama_mapel }}
-                                        </td>
-
-                                        <td class="text-center">
-                                            {{ Carbon\Carbon::parse($jadwal->jam_mulai)->format('H:i') }} -
-                                            {{ Carbon\Carbon::parse($jadwal->jam_selesai)->format('H:i') }}
-                                        </td>
-                                        <td class="text-center">
-                                            {{ $jadwal->ruang->nama_ruang }}
-                                        </td>
-
-                                        <td class="text-center">
-                                            {{ $jadwal->keterangan ?? 'Tidak ada' }}
-                                        </td>
-
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 @endsection
 {{-- footer --}}
