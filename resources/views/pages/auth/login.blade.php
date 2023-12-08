acd6b52f70cc24b78c9ac93ee6b5458e767f0eb2
Rynare <rynare.of.legend@gmail.com>
2023-12-08 15:34:36 +0700

    mengganti logo web

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}">
-    <link rel="icon" type="image/png" href="{{ asset('assets/img/web-icon-brain.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/smart scholl only icon.png') }}">
 
     <title>
         Smart School
